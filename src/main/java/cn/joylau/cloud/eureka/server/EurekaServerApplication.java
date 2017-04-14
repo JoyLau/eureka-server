459f58d83dac3bf63aea66790a73aea2b39f96b3
LiuFa_work <2587038142@qq.com>
2017-04-14 16:50:11 +0800

    Initial commit

---
+++ src/main/java/cn/joylau/cloud/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package cn.joylau.cloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
